a2e5b2e403bddba7c3aa5546813c5a59a7c6f8d2
AlexSt14 <78208403+AlexSt14@users.noreply.github.com>
2024-01-11 22:05:19 +0000

    Implementation of binary search algorithm in JavaScript

---
+++ binary search algorithm/binary.js
@@ -0,0 +1,54 @@
+function genRandomArray(n) {
+	var arr = [];
+	for (var i = 0; i < n; i++) {
+		arr[i] = Math.round(10 * Math.random());
+	}
+	return arr;
+}
+
+function swap(array, index1, index2) {
+	var saveElement = array[index1];
+	array[index1] = array[index2];
+	array[index2] = saveElement;
+	return array;
+}
+
+function bubbleSort(array) {
+	var n = array.length;
+	for (var i = 1; i < n; i++) {
+		var count = 0;
+		for (var j = 0; j < n - 1; j++) {
+			if (array[j + 1] < array[j]) {
+				count++;
+				swap(array, j, j + 1);
+			}
+		}
+		if (count == 0) {
+			break;
+		}
+	}
+	return array;
+}
+
+function binarySearch(array, x) {
+    var n = array.length;
+    var L = 0;
+    var R = n;
+    while (R >= L) {
+        var M = Math.floor((L+R)/2);
+        if (array[M] == x) {
+            return true;
+        } 
+        else if (array[M] > x) {
+            R = M-1;
+        }
+        else {
+            L = M+1;
+        }
+    }
+    return false;
+}
+
+var arr = genRandomArray(14);
+console.log(bubbleSort(arr));
+console.log(binarySearch(bubbleSort(arr), 7));
