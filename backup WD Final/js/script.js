aa130aceabc0a74c40fde9379b4b7dca41c0e8e9
AlexSt14 <78208403+AlexSt14@users.noreply.github.com>
2024-02-14 10:32:56 +0000

    WD Final Best Deals page

--- backup WD Final/js/script.js
+++ backup WD Final/js/script.js
@@ -54,4 +54,66 @@ window.onload = function() {
         }
     }
     //END script
+    
+     
 };
+
+//Slideshow script for TopGPU page THE 1440p SLIDESHOW
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activee", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " activee";
+}
+
+//Slideshow script for TopGPU page the 4k SLIDESHOW
+let slideIndex4k = 1;
+showSlides4k(slideIndex4k);
+
+// Next/previous controls
+function plusSlides4k(n) {
+    showSlides4k(slideIndex4k += n);
+}
+
+// Thumbnail image controls
+function currentSlide4k(n) {
+    showSlides4k(slideIndex4k = n);
+}
+
+function showSlides4k(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides2");
+    let dots = document.getElementsByClassName("dot2");
+    if (n > slides.length) {slideIndex4k = 1}
+    if (n < 1) {slideIndex4k = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activee", "");
+    }
+    slides[slideIndex4k-1].style.display = "block";
+    dots[slideIndex4k-1].className += " activee";
+}
